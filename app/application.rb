3f6a843f3e5e5b37424ea239d9957f9229dea834
github username <github email address>
2019-06-23 22:03:41 -0400

    Using rack to establish multiple paths with specific search req parameters

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -15,6 +16,26 @@ class Application
       resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
+    end
+
+    if req.path.match(/cart/) && @@cart.any?
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    else
+      resp.write "Your cart is empty."
+    end
+
+    if req.path.match(/add/)
+      search_term2 = req.params["item"]
+      if @@items.include?(search_term2)
+        @@cart << search_term2
+        resp.write "added #{search_term2}"
+      else
+        resp.write "We don't have that item"
+      end
+    else
+      resp.write "Path not found"
     end
 
     resp.finish
